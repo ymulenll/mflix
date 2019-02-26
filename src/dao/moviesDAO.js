870d78547376facc54af4bac618197b9fc375b19
Yoelvis Mulen <ymulenll@gmail.com>
2019-02-26 10:30:14 -0300

    chapter 2 implementation

--- src/dao/moviesDAO.js
+++ src/dao/moviesDAO.js
@@ -200,6 +200,9 @@ export default class MoviesDAO {
       sortStage,
       // TODO Ticket: Faceted Search
       // Add the stages to queryPipeline in the correct order.
+      skipStage,
+      limitStage,
+      facetStage
     ]
 
     try {
@@ -263,7 +266,7 @@ export default class MoviesDAO {
 
     // TODO Ticket: Paging
     // Use the cursor to only return the movies that belong on the current page
-    const displayCursor = cursor.limit(moviesPerPage)
+    const displayCursor = cursor.limit(moviesPerPage).skip(page * moviesPerPage);
 
     try {
       const moviesList = await displayCursor.toArray()
@@ -301,6 +304,31 @@ export default class MoviesDAO {
         {
           $match: {
             _id: ObjectId(id)
+          }
+        },
+        {
+          '$lookup': {
+            'from': 'comments',
+            'let': {
+              'id': '$_id'
+            },
+            'pipeline': [
+              {
+                '$match': {
+                  '$expr': {
+                    '$eq': [
+                      '$movie_id', '$$id'
+                    ]
+                  }
+                }
+              },
+              {
+                '$sort': {
+                  'date': -1
+                }
+              }
+            ],
+            'as': 'comments'
           }
         }
       ]
